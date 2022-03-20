4e2e29edb56db96611bad8e294a96f70006242e5
gfw-breaker <lixiaoming0513@gmail.com>
2022-03-20 03:10:48 -0400

    ok

---
+++ data/cert/generate.sh
@@ -0,0 +1,2 @@
+domain='docker-vray'
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout  /var/cert/server.key -out  /var/cert/server.crt -subj "/C=CN/ST=$domain/L=$domain/O=$domain/OU=$domain/CN=$domain"
