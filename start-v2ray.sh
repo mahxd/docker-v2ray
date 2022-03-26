62eb8fb4c02e5dcbac2193ec7e7efc69f5daba63
gfw-breaker <lixiaoming0513@gmail.com>
2022-03-25 23:36:06 -0400

    ok

--- start-v2ray.sh
+++ start-v2ray.sh
@@ -9,8 +9,8 @@ systemctl stop firewalld
 systemctl disable firewalld 
 
 # show attributes
-ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 nic=$(ip link | egrep ' ens| eth' | cut -d':' -f2)
+ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 uuid=$(cat data/v2ray/config.json | grep '"id"' | cut -d'"' -f4)
 path=$(cat data/nginx/conf.d/v2ray.conf | grep location | grep -v '/ ' | awk '{print $2}')
 
