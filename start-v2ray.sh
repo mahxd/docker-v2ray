0248fa25ceb9b42c1b5e2184d1cc37aab611c2cd
Mahdi Hoseini <85416927+mahxd@users.noreply.github.com>
2022-11-14 21:32:50 +0330

    get and show public IP

--- start-v2ray.sh
+++ start-v2ray.sh
@@ -10,12 +10,13 @@ systemctl disable firewalld
 
 # show attributes
 nic=$(ip link | egrep ' ens| eth| enp' | cut -d':' -f2)
-ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+#ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+ip=$(curl -4 -q icanhazip.com)
 uuid=$(cat data/v2ray/config.json | grep '"id"' | cut -d'"' -f4)
 path=$(cat data/nginx/conf.d/v2ray.conf | grep location | grep -v '/ ' | awk '{print $2}')
 
 echo
-echo "v2ray客户端参数："
+echo "v2ray VMESS："
 echo -e "addr:\t$ip"
 echo -e "id:\t$uuid"
 echo -e "path:\t$path"
