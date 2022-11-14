f1be081d314db2bfe39aa5e56f89e81175625d57
Mahdi Hoseini <85416927+mahxd@users.noreply.github.com>
2022-11-14 21:49:09 +0330

    add alert ID to be used in oneclick

--- start-v2ray.sh
+++ start-v2ray.sh
@@ -10,15 +10,19 @@ systemctl disable firewalld
 
 # show attributes
 nic=$(ip link | egrep ' ens| eth| enp' | cut -d':' -f2)
+
 #ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
 ip=$(curl -4 -q icanhazip.com)
-uuid=$(cat data/v2ray/config.json | grep '"id"' | cut -d'"' -f4)
+
+#uuid=$(cat data/v2ray/config.json | grep '"id"' | cut -d'"' -f4)
+uuid=$(cat data/v2ray/config.json | grep '"id"\|"alterId"')
+
 path=$(cat data/nginx/conf.d/v2ray.conf | grep location | grep -v '/ ' | awk '{print $2}')
 
 echo
 echo "v2ray VMESS："
 echo -e "addr:\t$ip"
-echo -e "id:\t$uuid"
+echo -e "$uuid"
 echo -e "path:\t$path"
 echo
 
